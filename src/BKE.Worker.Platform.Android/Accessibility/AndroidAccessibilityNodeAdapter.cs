ea2b1ee77603d6aaca49d2f0cb4a10f098a38835
jan2xo <163499503+jan2xo@users.noreply.github.com>
2026-09-01 15:23:25 -0700

    android: converge .NET binding APIs

--- src/BKE.Worker.Platform.Android/Accessibility/AndroidAccessibilityNodeAdapter.cs
+++ src/BKE.Worker.Platform.Android/Accessibility/AndroidAccessibilityNodeAdapter.cs
@@ -5,23 +5,26 @@ namespace BKE.Worker.Platform.Android.Accessibility;
 
 public sealed class AndroidAccessibilityNodeAdapter(AccessibilityNodeInfo node) : IAccessibilityNode
 {
+    private const string ActionArgumentSetTextCharSequence = "ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE";
+
     public string? Text => node.Text?.ToString();
     public string? ContentDescription => node.ContentDescription?.ToString();
     public string? ResourceId => node.ViewIdResourceName;
-    public bool IsClickable => node.IsClickable;
-    public bool IsEditable => node.IsEditable;
+    public bool IsClickable => node.Clickable;
+    public bool IsEditable => node.Editable;
     public IReadOnlyList<IAccessibilityNode> Children =>
         Enumerable.Range(0, node.ChildCount)
-            .Select(node.GetChild)
+            .Select(index => node.GetChild(index))
             .Where(child => child is not null)
             .Select(child => (IAccessibilityNode)new AndroidAccessibilityNodeAdapter(child!))
             .ToArray();
 
-    public bool Click() => node.PerformAction(AccessibilityNodeInfo.ActionClick);
+    public bool Click() => node.PerformAction(global::Android.Views.Accessibility.Action.Click);
+
     public bool SetText(string text)
     {
         var args = new Bundle();
-        args.PutCharSequence(AccessibilityNodeInfo.ActionArgumentSetTextCharSequence, text);
-        return node.PerformAction(AccessibilityNodeInfo.ActionSetText, args);
+        args.PutCharSequence(ActionArgumentSetTextCharSequence, text);
+        return node.PerformAction(global::Android.Views.Accessibility.Action.SetText, args);
     }
 }
