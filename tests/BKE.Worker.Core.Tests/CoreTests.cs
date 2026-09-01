10c3d1e40973781d415f7e6303f656ba18d9ac0c
jan2xo <163499503+jan2xo@users.noreply.github.com>
2026-09-01 15:09:23 -0700

    tests: import xUnit test attributes

--- tests/BKE.Worker.Core.Tests/CoreTests.cs
+++ tests/BKE.Worker.Core.Tests/CoreTests.cs
@@ -1,8 +1,15 @@
+global using Xunit;
 using BKE.Worker.Core;
+
 namespace BKE.Worker.Core.Tests;
+
 public class CoreTests
 {
     [Fact] public void Default_reasoning_is_high() => Assert.Equal(ReasoningProfile.HIGH, new WorkerPolicy().DefaultReasoning);
-    [Theory] [InlineData(ContextTargetType.RecentChat)] [InlineData(ContextTargetType.ProjectChat)] [InlineData(ContextTargetType.NewChat)] public void Context_targets_are_supported(ContextTargetType type) => Assert.Equal(type, new ContextTarget(type).Type);
+    [Theory]
+    [InlineData(ContextTargetType.RecentChat)]
+    [InlineData(ContextTargetType.ProjectChat)]
+    [InlineData(ContextTargetType.NewChat)]
+    public void Context_targets_are_supported(ContextTargetType type) => Assert.Equal(type, new ContextTarget(type).Type);
     [Fact] public void Default_resolves_to_policy() => Assert.Equal(ReasoningProfile.HIGH, new ReasoningResolver().Resolve(ReasoningProfile.DEFAULT, new WorkerPolicy()));
 }
